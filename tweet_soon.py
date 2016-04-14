eb0f20c15bbb80a5fc0ecc57756a76d3119ea5d5
ティン・ルーフ <tinruufu@gmail.com>
2016-04-14 21:55:19 +0100

    be occasional

---
+++ tweet_soon.py
@@ -0,0 +1,12 @@
+from time import sleep
+from random import random
+
+
+def tweet_soon():
+    sleep(60 * 60 * random())
+    from tweet import tweet
+    tweet()
+
+
+if __name__ == '__main__':
+    tweet_soon()
