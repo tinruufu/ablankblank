9f62f587533c27aa6f2d02c2038c215fae80abf3
ティン・ルーフ <tinruufu@gmail.com>
2016-04-14 21:04:11 +0100

    make pictures

---
+++ image.py
@@ -0,0 +1,27 @@
+import json
+import os
+from tempfile import mkstemp, mkdtemp
+
+from selenium.webdriver import PhantomJS
+
+from blank import get_structure
+
+
+def generate_image(structure):
+    image_path = os.path.join(mkdtemp(), 'okc.png')
+    html_path = os.path.join(
+        os.path.dirname(os.path.realpath(__file__)),
+        'okc.html',
+    )
+    url = 'file://{}'.format(html_path)
+    driver = PhantomJS(service_log_path=mkstemp()[1])
+    driver.set_window_size(2000, 500)
+    driver.get(url)
+    driver.execute_script('setText({});'.format(json.dumps(structure)))
+    driver.set_window_size(*driver.execute_script('return getSize();'))
+    driver.save_screenshot(image_path)
+    return image_path
+
+
+if __name__ == '__main__':
+    generate_image([segment.context() for segment in get_structure()])
